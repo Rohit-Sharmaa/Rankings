21ca9480efa2b17765a9a284f1792facd88c3433
Rohit-Sharmaa <iamrohit3000@gmail.com>
2024-09-03 12:59:35 +0530

    connected to db

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 import express from "express";
 import authRouter from "./routes/auth.routes.js";
+import dotenv from "dotenv";
 const PORT = 3000;
+import connectDb from "./config/db.js";
 const app = express();
+
+dotenv.config();
+connectDb();
 
 app.get("/", (req, res) => {
   res.send("Api is running");
