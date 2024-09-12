be97da17bbaada5c5533ef8ef0e8ddd1604a806d
sahil gupta <8278087870sahil@gmail>
2024-09-12 11:24:45 +0530

    feat: create sidebar & CSS

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,8 +1,12 @@
 import "./navbar.css";
 import logo from "../../assests/7.jpg";
 import data from "./data.js";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { MdMenuOpen } from "react-icons/md";
+import { useState } from "react";
 function Navbar() {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="nav_container">
@@ -15,19 +19,49 @@ function Navbar() {
         <ul className="nav_menu">
           {data.map((item) => (
             <li key={item.id}>
-              <Link to={item.link} className="nav_list_item">
+              <NavLink
+                to={item.link}
+                className={({ isActive }) =>
+                  `nav_list_item ${isActive ? "active" : ""}`
+                }
+              >
                 {item.title}
-              </Link>
+              </NavLink>
             </li>
           ))}
         </ul>
+
         <div className="login_button">
           {/* <button className="btn light theme">
             <MdOutlineLightMode />
           </button> */}
           <button className="btn white sm">Login</button>
         </div>
+
         <div>{/* sign up and sign in button  */}</div>
+
+        {/* Menu for smart phone*/}
+        <div>
+          <MdMenuOpen
+            className="m_nav_icon"
+            onClick={() => setToggle(!toggle)}
+            fontSize={24}
+          />
+          <div
+            onClick={() => setToggle(!toggle)}
+            className={`${!toggle ? "m_nav_hidden " : "m_nav_menu"} `}
+          >
+            <ul className="m_nav_menu_ul">
+              {data.map((item) => (
+                <li key={item.id}>
+                  <Link to={item.link} className="m_nav_menu_ul_link">
+                    {item.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   );
--- frontend/src/components/navbar/data.js
+++ frontend/src/components/navbar/data.js
@@ -1,9 +1,9 @@
 const data = [
   { id: 1, link: "/", title: "Profiles" },
   { id: 2, link: "/", title: "Analyze" },
-  { id: 3, link: "/contest", title: "upcoming contest" },
+  { id: 3, link: "/contest", title: "Upcoming Contest" },
   { id: 4, link: "/", title: "About" },
-  { id: 5, link: "/", title: "contributer" },
+  { id: 5, link: "/", title: "Contributor" },
 ];
 
 export default data;
