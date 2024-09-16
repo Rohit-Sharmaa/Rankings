63acd430cfc345879322a168639a0ca83b30e9af
sahil gupta <8278087870sahil@gmail>
2024-09-16 11:59:32 +0530

    Docs: put apilimit on coding profile

--- backend/routes/codingProfiles.route.js
+++ backend/routes/codingProfiles.route.js
@@ -5,13 +5,14 @@ import { geeksforgeeks } from "../controllers/geeksforgeeks.js";
 import { codechef } from "../controllers/codechef.js";
 import { codeforces } from "../controllers/codeforces.js";
 import { getUserProfile } from "../controllers/getUserProfile.js";
+import apiLimit from "../utils/ApiRateLimiter.js"
 const router = express.Router();
 
 //i have to make this route protected
-router.post("/leetcode", verifyToken, leetcode);
-router.post("/geeksforgeeks", verifyToken, geeksforgeeks);
-router.post("/codechef", verifyToken, codechef);
-router.post("/codeforces", verifyToken, codeforces);
+router.post("/leetcode",apiLimit, verifyToken, leetcode);
+router.post("/geeksforgeeks",apiLimit, verifyToken, geeksforgeeks);
+router.post("/codechef",apiLimit, verifyToken, codechef);
+router.post("/codeforces",apiLimit, verifyToken, codeforces);
 
 router.get("/getUserProfile", verifyToken, getUserProfile);
 
