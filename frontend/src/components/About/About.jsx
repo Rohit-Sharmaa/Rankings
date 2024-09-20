ddf6e896560649bfeb042313fefcd44ffdbb2455
Rohit-Sharmaa <iamrohit3000@gmail.com>
2024-09-21 01:11:37 +0530

    fix: fixed loader error

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./About.css";
+import "./about.css";
 
 const About = () => {
   return (
@@ -7,37 +7,49 @@ const About = () => {
       <section className="about-section">
         <h1>About Us</h1>
         <p>
-          At our core, we simplify the process of tracking and comparing your coding performance across multiple platforms. Whether it’s Codeforces, LeetCode, or others, we bring your rankings together in one place.
+          At our core, we simplify the process of tracking and comparing your
+          coding performance across multiple platforms. Whether it’s Codeforces,
+          LeetCode, or others, we bring your rankings together in one place.
         </p>
         <p>
-          Our mission is to empower developers by providing real-time insights into their competitive coding journey, enabling you to monitor your growth and stay ahead of the competition.
+          Our mission is to empower developers by providing real-time insights
+          into their competitive coding journey, enabling you to monitor your
+          growth and stay ahead of the competition.
         </p>
         <p>
-          We take the time to understand each user's needs and deliver customized solutions to enhance their experience.
+          We take the time to understand each user's needs and deliver
+          customized solutions to enhance their experience.
         </p>
         <p>
-          Backed by a talented team of full-stack developers, we combine the strengths of our front-end,back-end and full-stack specialists to create seamless, responsive, and powerful applications tailored just for you.
+          Backed by a talented team of full-stack developers, we combine the
+          strengths of our front-end,back-end and full-stack specialists to
+          create seamless, responsive, and powerful applications tailored just
+          for you.
         </p>
       </section>
 
-
       <section className="about-section">
-        <h1><span>Why Choose Us ?</span></h1>
+        <h1>
+          <span>Why Choose Us ?</span>
+        </h1>
+        <p>The ultimate hub for coders seeking challenges!</p>
         <p>
-        The ultimate hub for coders seeking challenges!
+          We aggregate top coding contests from around the globe, making it
+          easier for you to find and participate in competitive programming
+          events.
         </p>
         <p>
-        We aggregate top coding contests from around the globe, making it easier for you to find and participate in competitive programming events.
+          We also offer profile synchronization from multiple coding platforms,
+          allowing you to easily compare your coding stats and track your
+          progress across different platforms.
         </p>
         <p>
-        We also offer profile synchronization from multiple coding platforms, allowing you to easily compare your coding stats and track your progress across different platforms.
-       </p>
-       <p>
-        Driven by passion and a commitment to excellence, we dedicated ourselves to this vision. After countless hours of hard work and perseverance, we’ve brought it to life, and we're just getting started.
-       </p>
-        <p>
-        Join us and make your coding journey exciting and rewarding! 
+          Driven by passion and a commitment to excellence, we dedicated
+          ourselves to this vision. After countless hours of hard work and
+          perseverance, we’ve brought it to life, and we're just getting
+          started.
         </p>
+        <p>Join us and make your coding journey exciting and rewarding!</p>
       </section>
     </div>
   );
